cb8c0a81627b242ccd7ca775108bec079b726083
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2023-04-24 21:16:53 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "
++----------------------------------------------------------------------
+| V2Board-FreeNode Dev For V2Board 1.7.3
++----------------------------------------------------------------------
+| Copyright © 2020-2023 SANYIMOE Inc. All rights reserved.
++----------------------------------------------------------------------
+| The FreeNode file in /resources/free.json and the FreeNode flag=free
++----------------------------------------------------------------------
+"
+
+while [ "$site_directory" == null ]; do
+    read -p "Please enter your site directory:" site_directory
+done
+
+echo "Your site directory is: $site_directory"
